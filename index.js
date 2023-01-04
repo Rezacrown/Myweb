e8af42a0d616523d034456ec4c1a0f9f30ac4cfd
Rezacrown <aryhidayatfebriana@gmail.com>
2023-01-04 00:26:29 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnnav = document.getElementById('btnnav')
+const navmenu = document.getElementById('navmenu')
+
+btnnav.addEventListener('click', function(){
+
+    navmenu.classList.toggle('hidden')
+
+})
